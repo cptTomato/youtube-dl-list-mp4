22445aaa35be2b7ee3a5bf36b16bf16df5f38331
cptTomato <55293427+cptTomato@users.noreply.github.com>
2019-09-17 12:51:32 +0200

    Update you.py

--- you.py
+++ you.py
@@ -1,8 +1,13 @@
-data = open("list.txt", "a")
+#import operating system for commands
 import os
 
+#reading links from list.txt
+data = open("list.txt", "a")
+
+#getting links from list.txt
 for line in daten:
     y = (line.rstrip())
-    os.system("sudo youtube-dl " + y)
+    # download the videos
+    os.system("sudo youtube-dl -f mp4 " + y)
 
 data.close()
